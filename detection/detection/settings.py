9001a1df2b6f8bf23b723003afa3a61d1b172295
Rabana21 <rbnbullones1@uma.es>
2022-09-13 10:13:08 +0200

    fix: Template path doesnt exit, change to BASE_DIR

--- detection/detection/settings.py
+++ detection/detection/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'detection.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["/home/ruben/Documentos/torchserve/detection/templates"],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
